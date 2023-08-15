89135b2912b53dd1913b00392b9e63fe5fb590e3
Mohit <pahariyamohit9@gmail.com>
2023-08-15 16:54:29 +0530

    bugged free

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const fs = require("fs");
 const contactSchema = new mongoose.Schema({
     name: String,
     phone: String,
-    address: String,
     email: String,
+    about: String,
   });
 
 const Contact = mongoose.model('Contact', contactSchema);
@@ -32,7 +32,7 @@ const Contact = mongoose.model('Contact', contactSchema);
 //for express
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug') // Set the template engine as pug
@@ -47,18 +47,6 @@ app.get('/contact', (req, res)=>{
     const params = {}
     res.status(200).render('contact.pug', params);
 })
-// app.post('/contact', (req, res)=>{
-//     name = req.body.name
-//     phone = req.body.phone
-//     email = req.body.email
-//     about = req.body.about
-
-//     let outputToWrite = `the name of the client is ${name}, ${phone} years old, ${email}. More about him/her: ${about}`
-//     fs.writeFileSync('output.txt', outputToWrite)
-//     const params = {'message': 'Your form has been submitted successfully'}
-//     res.status(200).render('contact.pug', params);
-
-// })
 
 app.post('/contact', (req, res)=>{
     var myData = new Contact(req.body);
@@ -67,7 +55,7 @@ app.post('/contact', (req, res)=>{
     }).catch(()=>{
     res.status(400).send('item was not saved to the databse')
 })
-// res.status(200).render('contact.pug');
+res.status(200).render('index.pug');
 })
 
 //START THE SERVER
---
+++ app2.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8000;
+const fs = require("fs");
+const bodyparser = require("body-parser");
+
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded({ extended: true }))
+
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+app.post('/contact', (req, res)=>{
+    
+
+    name = req.body.name
+    phone = req.body.phone
+    email = req.body.email
+    about = req.body.about
+    
+
+    let outputToWrite = `The name of the client is "${name}" \nEmail is "${email}"\nPhone no is "${phone} " \nCondition and goals: "${about}"`
+    // fs.writeFileSync('output.txt', outputToWrite);
+    fs.appendFileSync('output.txt', '\n' + outputToWrite );
+    
+
+ const params = {'message': 'Your form has been submitted successfully'}
+ res.status(200).render('index.pug', params);
+});
+
+//START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
